dc03826d8cacac1591bda36d545943901ca25d82
joeypanganiban <joeympanganiban30@gmail.com>
2021-01-09 23:00:54 +0800

    update v4

--- js/main.js
+++ js/main.js
@@ -1,2 +1,3 @@
-const options = {day: 'numeric', month: 'long', year: 'numeric'};
+const options = {day: 'numeric', month: 'long', year: 'numeric', hour: "numeric", minute: "numeric"};
         document.getElementById("date").textContent = new Date().toLocaleDateString('en-US', options);
+
