91497cce71d8aaa467a76e34fd5237c880c0b79b
joeypanganiban <joeympanganiban30@gmail.com>
2021-01-16 10:21:25 +0800

    update v4.2

--- lesson2/js2/footer.js
+++ lesson2/js2/footer.js
@@ -1,4 +1,4 @@
-const options = {day: 'numeric', month: 'long', year: 'numeric', hour: "numeric", minute: "numeric"};
-        document.getElementById("date").textContent = new Date().toLocaleDateString('en-US', options);
-const currentYear ={year: 'numeric'}
-        document.getElementById("year").textContent = new Date().toLocaleDateString('en-US', currentYear);
+const options2 = {day: 'numeric', month: 'long', year: 'numeric', hour: "numeric", minute: "numeric"};
+        document.getElementById("date2").textContent = new Date().toLocaleDateString('en-US', options2);
+const currentYear2 ={year: 'numeric'}
+        document.getElementById("year2").textContent = new Date().toLocaleDateString('en-US', currentYear2);
