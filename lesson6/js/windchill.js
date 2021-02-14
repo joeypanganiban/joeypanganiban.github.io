fb6fc23ac28538180456c859dd460c24dc9fc0b1
joeypanganiban <joeympanganiban30@gmail.com>
2021-02-14 12:41:38 +0800

    lesson 6 v9

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -9,6 +9,8 @@
         let f = 35.74 + 0.6215 * t 
                         - 35.75 * Math.pow(s, 0.16)
                         + 0.4275 * t * Math.pow(s, 0.16);
-
-        return Math.round(f);   
+        let digits = 2;
+        let multiplier = Math.pow(10, digits);
+        let total = Math.round(f * multiplier) / multiplier
+        return total;   
     }
