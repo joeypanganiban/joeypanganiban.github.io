1b578bbb14e2866e769d30a0d9861bf268dc7ea1
joeypanganiban <joeympanganiban30@gmail.com>
2021-04-03 19:43:34 +0800

    cc 7

--- ChamberOfCommerce/js/script.js
+++ ChamberOfCommerce/js/script.js
@@ -18,3 +18,17 @@ var slideIndex = 0;
         dots[slideIndex-1].className += " active";
         setTimeout(showSlides, 5000); // Change image every 2 seconds
     }
+
+// Upcoming Events
+const eventsAPI = "https://joeypanganiban.github.io/ChamberOfCommerce/events.json"
+fetch(eventsAPI)
+      .then(function (response) {
+          return response.json();
+      })
+      .then(function (jsonObject) {
+        //console.table(jsonObject); // temporary checking for valid response and data parsing
+        const towns = jsonObject['events'];
+        // select output location
+        const events = document.querySelector('.upcomingEvents');
+        // filter the towns
+        });
