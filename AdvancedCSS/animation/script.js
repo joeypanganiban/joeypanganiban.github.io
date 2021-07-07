3707f740b99c2de01e0dd99ae14285f51df3e5e0
joeypanganiban <joeympanganiban30@gmail.com>
2021-07-08 06:47:07 +0800

    yes

---
+++ AdvancedCSS/animation/script.js
@@ -0,0 +1,6 @@
+function toggleNav() {
+    var openSlides = document.getElementById("menu-icon");
+        openSlides.classList.toggle("open");
+    var navSlides = document.getElementById("nav");
+        navSlides.classList.toggle("open");
+}
